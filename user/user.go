65c28b65b2e94a86fe0f5a9a99986b06ad4f4b7b
Jordan Wood <jordan@elitehosting.io>
2019-07-15 23:30:01 +0100

    Easy fix

--- user/user.go
+++ user/user.go
@@ -91,7 +91,7 @@ func userHandler(c *gin.Context) []byte {
 		matchGame += ", \"game_mode_id\" : \"" + request.Mode + "\""
 	}
 
-	pipeline = fmt.Sprintf(pipeline, request.ID, "TODO" , request.ID, request.Game)
+	pipeline = fmt.Sprintf(pipeline, request.ID, matchGame , request.ID, request.Game)
 
 	var gameModeUserResponse gameModeUserResponse
 	gameModeUserResponse.EventTotals = make(map[string]map[string]map[string]int64)
